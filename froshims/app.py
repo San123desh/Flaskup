71b1b00cb8bca8244c2b81cd1965de2241cea792
San123desh <suuchang53@gmail.com>
2024-07-13 22:17:20 +0545

    validation

--- froshims/app.py
+++ froshims/app.py
@@ -2,10 +2,15 @@ from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
+SPORTS = ["Football","Crickte","Badmintion"]
 
 @app.route('/', methods=['GET', 'POST'])
 def hello_world():
-    if request.method == 'POST':
-        name = request.form.get['name']
-        return render_template('greet.html', name=name)
-    return render_template('index.html')
+    return render_template('index.html', sport=SPORTS)
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    if not request.form.get("name") or request.form.get("sport") not in SPORTS:
+        return render_template('failure.html')
+    return render_template('success.html')
